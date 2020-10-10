5f0a476c05917cb5a5c42af06976339156c58b78
seosaju <qua_ver@naver.com>
2020-10-10 20:38:57 +0900

    Actor 모델 django-admin 연동

--- face_recognition/models.py
+++ face_recognition/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Actor(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
