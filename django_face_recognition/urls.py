30e5a152435b439c9521ce77aeb3a4f58d0cf313
seosaju <qua_ver@naver.com>
2020-10-10 20:38:32 +0900

    face_recognition url 연동

--- django_face_recognition/urls.py
+++ django_face_recognition/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('face_recognition'))
+    path('', include('face_recognition.urls'))
 ]
 
 if settings.DEBUG:
