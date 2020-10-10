b75536299d0bf08f46ed59852d9fa1db78f6b5ee
seosaju <qua_ver@naver.com>
2020-10-10 20:39:48 +0900

    이미지 띄워주는 페이지 제작

--- face_recognition/urls.py
+++ face_recognition/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
+from face_recognition import views
+
 app_name = 'face_recognition'
 
 urlpatterns = [
-
+    path('', views.ActorImageTV.as_view(), name='index'),
+    path('actor/<int:pk>', views.ActorDisplayDV.as_view(), name='display'),
 ]
--- face_recognition/views.py
+++ face_recognition/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, DetailView
 
-# Create your views here.
+from face_recognition.forms import ActorForm
+from face_recognition.models import Actor
+
+
+class ActorImageTV(TemplateView):
+    form = ActorForm
+    template_name = 'face_recognition/upload_image.html'
+
+    def post(self, request, *args, **kwargs):
+        form = ActorForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save()
+            return HttpResponseRedirect(reverse_lazy('face_recognition:display', kwargs={'pk': obj.id}))
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
+class ActorDisplayDV(DetailView):
+    model = Actor
+    template_name = 'face_recognition/show_image.html'
+    context_object_name = 'actor'
