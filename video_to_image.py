9b10b0e21161a7713a9be5d3bb784af82147eb1a
seosaju <qua_ver@naver.com>
2020-11-09 16:18:37 +0900

    얼굴이 없는 이미지 스킵

--- video_to_image.py
+++ video_to_image.py
@@ -1,26 +1,34 @@
 import os
 
 import cv2
+import dlib
 
 
 def video_to_image(video_path, directory):
     video = cv2.VideoCapture(video_path)
+    face_detector = dlib.get_frontal_face_detector()
     dirname = str(directory).split("/")[4]
     os.mkdir(f"media/images/original/videos/{dirname}")
 
     def get_frame(sec):
         video.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
         has_frames, image = video.read()
-        if has_frames:
-            cv2.imwrite(f"{directory}/image_{str(count)}.jpg", image)     # save frame as JPG file
+        nonlocal i
+        try:
+            detected_face = face_detector(image)
+        except TypeError:
+            return
+        else:
+            if list(detected_face):
+                cv2.imwrite(f"{directory}/image_{str(i)}.jpg", image)     # save frame as JPG file
+                i = i + 1
         return has_frames
 
     second = 0
-    frame_rate = 3  # it will capture image in each 5 second
-    count = 1
+    frame_rate = 3  # it will capture image in each 3 second
+    i = 1
     frame = get_frame(second)
     while frame:
-        count = count + 1
         second = second + frame_rate
         second = round(second, 2)
         frame = get_frame(second)
